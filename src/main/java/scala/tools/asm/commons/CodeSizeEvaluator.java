310ebd765ef018f3a6d6ffdb4ce00ec228168b1a
Jason Zaugg <jzaugg@gmail.com>
2018-06-07 11:39:28 +1000

    Update package clauses

--- src/main/java/scala/tools/asm/commons/CodeSizeEvaluator.java
+++ src/main/java/scala/tools/asm/commons/CodeSizeEvaluator.java
@@ -25,7 +25,7 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 // THE POSSIBILITY OF SUCH DAMAGE.
-package org.objectweb.asm.commons;
+package scala.tools.asm.commons;
 
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.Label;
