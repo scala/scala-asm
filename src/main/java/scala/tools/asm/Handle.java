f5172721bbf09e25b515fb378f7ee4cbc024655f
Lukas Rytz <lukas.rytz@gmail.com>
2022-04-21 15:23:34 +0200

    Update package clauses

--- src/main/java/scala/tools/asm/Handle.java
+++ src/main/java/scala/tools/asm/Handle.java
@@ -26,7 +26,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 // THE POSSIBILITY OF SUCH DAMAGE.
 
-package org.objectweb.asm;
+package scala.tools.asm;
 
 /**
  * A reference to a field or a method.
