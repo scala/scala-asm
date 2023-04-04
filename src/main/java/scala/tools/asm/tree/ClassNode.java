724112dd01d3b3bf893308f8764d3f3944093554
Seth Tisue <seth@tisue.net>
2023-04-04 17:04:13 +0200

    Update `@links`, `@associates`

--- src/main/java/scala/tools/asm/tree/ClassNode.java
+++ src/main/java/scala/tools/asm/tree/ClassNode.java
@@ -53,19 +53,19 @@ public class ClassNode extends ClassVisitor {
   public int version;
 
   /**
-   * The class's access flags (see {@link org.objectweb.asm.Opcodes}). This field also indicates if
+   * The class's access flags (see {@link scala.tools.asm.Opcodes}). This field also indicates if
    * the class is deprecated {@link Opcodes#ACC_DEPRECATED} or a record {@link Opcodes#ACC_RECORD}.
    */
   public int access;
 
-  /** The internal name of this class (see {@link org.objectweb.asm.Type#getInternalName()}). */
+  /** The internal name of this class (see {@link scala.tools.asm.Type#getInternalName()}). */
   public String name;
 
   /** The signature of this class. May be {@literal null}. */
   public String signature;
 
   /**
-   * The internal of name of the super class (see {@link org.objectweb.asm.Type#getInternalName()}).
+   * The internal of name of the super class (see {@link scala.tools.asm.Type#getInternalName()}).
    * For interfaces, the super class is {@link Object}. May be {@literal null}, but only for the
    * {@link Object} class.
    */
@@ -73,7 +73,7 @@ public class ClassNode extends ClassVisitor {
 
   /**
    * The internal names of the interfaces directly implemented by this class (see {@link
-   * org.objectweb.asm.Type#getInternalName()}).
+   * scala.tools.asm.Type#getInternalName()}).
    */
   public List<String> interfaces;
 
@@ -90,7 +90,7 @@ public class ClassNode extends ClassVisitor {
 
   /**
    * The internal name of the enclosing class of this class (see {@link
-   * org.objectweb.asm.Type#getInternalName()}). Must be {@literal null} if this class has no
+   * scala.tools.asm.Type#getInternalName()}). Must be {@literal null} if this class has no
    * enclosing class, or if it is a local or anonymous class.
    */
   public String outerClass;
@@ -131,19 +131,19 @@ public class ClassNode extends ClassVisitor {
 
   /**
    * The internal name of the nest host class of this class (see {@link
-   * org.objectweb.asm.Type#getInternalName()}). May be {@literal null}.
+   * scala.tools.asm.Type#getInternalName()}). May be {@literal null}.
    */
   public String nestHostClass;
 
   /**
    * The internal names of the nest members of this class (see {@link
-   * org.objectweb.asm.Type#getInternalName()}). May be {@literal null}.
+   * scala.tools.asm.Type#getInternalName()}). May be {@literal null}.
    */
   public List<String> nestMembers;
 
   /**
    * The internal names of the permitted subclasses of this class (see {@link
-   * org.objectweb.asm.Type#getInternalName()}). May be {@literal null}.
+   * scala.tools.asm.Type#getInternalName()}). May be {@literal null}.
    */
   public List<String> permittedSubclasses;
 
