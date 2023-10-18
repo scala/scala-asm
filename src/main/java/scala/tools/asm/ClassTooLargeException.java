8a4db2e0ddfd9cf1300f14472a1b2bef79b5a37a
Seth Tisue <seth@tisue.net>
2023-10-17 19:51:17 -0600

    Update package clauses

--- src/main/java/scala/tools/asm/ClassTooLargeException.java
+++ src/main/java/scala/tools/asm/ClassTooLargeException.java
@@ -25,7 +25,7 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 // THE POSSIBILITY OF SUCH DAMAGE.
-package org.objectweb.asm;
+package scala.tools.asm;
 
 /**
  * Exception thrown when the constant pool of a class produced by a {@link ClassWriter} is too
